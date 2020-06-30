4a83c1bf99097d618eaf5c69cf8e1d1819e0d3b0
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-06-30 18:15:47 -0500

    update account reducer and create fetchAccounts.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import {BrowserRouter as Router} from 'react-router-dom'
+import accountReducer from './reducers/accountReducer'
+
+import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-let store = createStore(composeEnhancers(applyMiddleware(thunk)))
+
+let store = createStore(accountReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
