2ba5b57ef5bc8dd41048f48163d12d0b8abd9006
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-07-01 01:34:26 -0500

    update accountReducer

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import React from 'react';
 import {connect} from 'react-redux'
 import AccountsContainer from './containers/AccountsContainer'
-import {fetchAccounts} from './actions/fetchAccounts'
 
 class App extends React.Component {
-
-  // componentDidMount() {
-  //   this.props.fetchAccounts({type: 'FETCH_ACCOUNTS', payload: {name:"Checking"}})
-  // }
 
   render() {
     return (
       <div className="App">
-        <AccountsContainer />
+        <AccountsContainer/>
       </div>
     );
   }
 }
 
-// const mapStateToProps = (state) {
-//     return {
-//       accounts: state.accounts
-//     }
-// }
 
 export default App;
